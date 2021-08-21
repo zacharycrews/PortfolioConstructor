4cbcd1847f578178eb2518e67f8e57b8f3cf975a
Zach Crews <crewsz@bc.edu>
2021-08-21 12:35:26 -0400

    Grouped all portfolio attributes and functions into one class

--- main.py
+++ main.py
@@ -4,60 +4,79 @@ import numpy
 import finsymbols
 import yfinance as yf
 
-# Calculates standard deviation of three-stock portfolio
-def portfolio_stdev(weights, returns, stdevs) -> float:
-    result = 0.0
-    for i in range(3):
-        result += math.pow(weights[i],2) * math.pow(stdevs[i],2)
-    result += 2 * numpy.corrcoef(returns[0],returns[1])[0][1] * weights[0] * weights[1] * stdevs[0] * stdevs[1]
-    result += 2 * numpy.corrcoef(returns[0], returns[2])[0][1] * weights[0] * weights[2] * stdevs[0] * stdevs[2]
-    result += 2 * numpy.corrcoef(returns[1], returns[2])[0][1] * weights[1] * weights[2] * stdevs[1] * stdevs[2]
-    return math.sqrt(result)
+class Portfolio:
 
-# Gets all companies in S&P 500 index
-companies = []
-for company in finsymbols.get_sp500_symbols():
-    companies.append(company['symbol'].strip('\n'))
+    def __init__(self, stocks):
+        self.stocks = stocks
+        self.weights = [1/3, 1/3, 1/3]
+        self.indv_returns = [[], [], []]
+        self.indv_stdevs = []
+        self.set_indv_attributes()
+        self.p_return = 0.0
+        self.p_stdev = 0.0
+        self.set_portfolio_return()
+        self.set_portfolio_stdev()
 
-# Prompts user to enter three stocks
-tickers = []
-i = 1
-while i < 4:
-    ticker = input(f"Enter ticker #{i}: ").strip('\n').upper()
-    if ticker in companies:
-        tickers.append(ticker)
-        i += 1
-    else:
-        print("Invalid ticker.")
 
-# Downloads historical price data for chosen stocks
-data = yf.download(tickers=tickers, period = "5y", interval="3mo")
+    def set_indv_attributes(self):
+        # Puts relevant prices into 2D array 'returns'
+        for i in range(len(self.stocks)):
+            for price in data['Close'][self.stocks[i]]:
+                if not math.isnan((price)):
+                    self.indv_returns[i].append(price)
+            self.indv_returns[i].pop()
 
-# Puts relevant prices into 2D array 'returns'
-returns = [[], [], []]
-for i in range(len(tickers)):
-    for price in data['Close'][tickers[i]]:
-        if not math.isnan((price)):
-            returns[i].append(price)
-    returns[i].pop()
+        # Iterates through returns, changing the actual prices to the percent change between periods
+        for company in self.indv_returns:
+            for i in range(len(company) - 1):
+                company.append((company[1] - company[0]) / company[0])
+                company.pop(0)
+            company.pop(0)
 
-# Iterates through returns, changing the actual prices to the percent change between periods
-for company in returns:
-    for i in range(len(company) - 1):
-        company.append((company[1] - company[0])/company[0])
-        company.pop(0)
-    company.pop(0)
+        # Calculate standard deviation in returns for each individual stock
+        for i in range(len(self.stocks)):
+            self.indv_stdevs.append(statistics.stdev(self.indv_returns[i]))
 
-# Summary statistics
-print("*** INDIVIDUAL ASSET CHARACTERISTICS ***\n")
-exp_ann = []
-stdevs = []
-for i in range(len(returns)):
-    exp_ann.append(math.pow(1+sum(returns[i])/len(returns[i]),4) - 1)
-    stdevs.append(statistics.stdev(returns[i]))
-    print(f"E[r] {tickers[i]}: {exp_ann[i]}")
-    print(f"σ(r) {tickers[i]}: {stdevs[i]}\n")
+    # Calculate expected annual return of three-stock portfolio
+    def set_portfolio_return(self):
+        annualized = 0.0
+        for i in range(3):
+            annualized += (math.pow(1 + sum(self.indv_returns[i]) / len(self.indv_returns[i]), 4) - 1) * self.weights[i]
+        self.p_return = annualized
 
-print("*** UNWEIGHTED PORTFOLIO CHARACTERISTICS ***\n")
-print(f"E[r]p: {sum(exp_ann)/len(exp_ann)}")
-print(f"σ(p): {portfolio_stdev([1/3,1/3,1/3], returns, stdevs)}")
+    # Calculates standard deviation of three-stock portfolio
+    def set_portfolio_stdev(self):
+        result = 0.0
+        for i in range(3):
+            result += math.pow(self.weights[i], 2) * math.pow(self.indv_stdevs[i], 2)
+        result += 2 * numpy.corrcoef(self.indv_returns[0], self.indv_returns[1])[0][1] * self.weights[0] * self.weights[1] * self.indv_stdevs[0] * self.indv_stdevs[1]
+        result += 2 * numpy.corrcoef(self.indv_returns[0], self.indv_returns[2])[0][1] * self.weights[0] * self.weights[2] * self.indv_stdevs[0] * self.indv_stdevs[2]
+        result += 2 * numpy.corrcoef(self.indv_returns[1], self.indv_returns[2])[0][1] * self.weights[1] * self.weights[2] * self.indv_stdevs[1] * self.indv_stdevs[2]
+        self.p_stdev = math.pow(1 + result, 4) - 1
+
+if __name__ == '__main__':
+    # Gets all companies in S&P 500 index
+    companies = []
+    for company in finsymbols.get_sp500_symbols():
+        companies.append(company['symbol'].strip('\n'))
+
+    # Prompts user to enter three stocks
+    tickers = []
+    i = 1
+    while i < 4:
+        ticker = input(f"Enter ticker #{i}: ").strip('\n').upper()
+        if ticker in companies:
+            tickers.append(ticker)
+            i += 1
+        else:
+            print("Invalid ticker.")
+
+    # Downloads historical price data for chosen stocks
+    data = yf.download(tickers=tickers, period = "5y", interval="3mo")
+
+    # Create Portfolio object out of 3 stocks
+    portfolio = Portfolio(tickers)
+
+    # Print portfolio characteristics
+    print(f"\n\nE[r]p: {portfolio.p_return}")
+    print(f"σ(p): {portfolio.p_stdev}")
